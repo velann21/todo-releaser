a2ad15d0706598ddc0c57ee749e05d0814fc7510
velann21 <velann21@gmail.com>
2026-01-02 13:41:29 +0100

    feat: added the working pulumi and ansible code

--- infrastructure/main.go
+++ infrastructure/main.go
@@ -1,13 +1,64 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/rds"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/rds"
+	"github.com/pulumi/pulumi-command/sdk/go/command/local"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+func ensureSSHKey(keyPath string) error {
+	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+		// Generate SSH key
+		cmd := exec.Command("ssh-keygen", "-t", "rsa", "-b", "4096", "-f", keyPath, "-N", "")
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+		// Ensure correct permissions
+		if err := os.Chmod(keyPath, 0400); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		// 0. Ensure SSH Key Exists
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		privateKeyPath := filepath.Join(homeDir, ".ssh", "todo-key.pem")
+		publicKeyPath := privateKeyPath + ".pub"
+
+		if err := ensureSSHKey(privateKeyPath); err != nil {
+			return err
+		}
+
+		publicKeyContent, err := os.ReadFile(publicKeyPath)
+		if err != nil {
+			return err
+		}
+
+		// Create AWS Key Pair
+		keyPair, err := ec2.NewKeyPair(ctx, "todo-key-pair", &ec2.KeyPairArgs{
+			PublicKey: pulumi.String(string(publicKeyContent)),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("todo-key-pair"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		// 1. Create a VPC
 		vpc, err := ec2.NewVpc(ctx, "todo-vpc", &ec2.VpcArgs{
 			CidrBlock:          pulumi.String("10.0.0.0/16"),
@@ -32,22 +83,49 @@ func main() {
 			return err
 		}
 
-		// Create a Public Subnet
-		subnet, err := ec2.NewSubnet(ctx, "todo-subnet", &ec2.SubnetArgs{
+		// 2. Subnets
+		// Public Subnet (for EC2)
+		publicSubnet, err := ec2.NewSubnet(ctx, "todo-public-subnet-1", &ec2.SubnetArgs{
 			VpcId:               vpc.ID(),
 			CidrBlock:           pulumi.String("10.0.1.0/24"),
 			MapPublicIpOnLaunch: pulumi.Bool(true),
-			AvailabilityZone:    pulumi.String("us-east-1a"), // Hardcoded for simplicity, should be dynamic
+			AvailabilityZone:    pulumi.String("eu-west-1a"),
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("todo-subnet"),
+				"Name": pulumi.String("todo-public-subnet-1"),
 			},
 		})
 		if err != nil {
 			return err
 		}
 
-		// Create Route Table
-		rt, err := ec2.NewRouteTable(ctx, "todo-rt", &ec2.RouteTableArgs{
+		// Private Subnet 1 (for RDS)
+		privateSubnet1, err := ec2.NewSubnet(ctx, "todo-private-subnet-1", &ec2.SubnetArgs{
+			VpcId:            vpc.ID(),
+			CidrBlock:        pulumi.String("10.0.2.0/24"),
+			AvailabilityZone: pulumi.String("eu-west-1a"),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("todo-private-subnet-1"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Private Subnet 2 (for RDS HA)
+		privateSubnet2, err := ec2.NewSubnet(ctx, "todo-private-subnet-2", &ec2.SubnetArgs{
+			VpcId:            vpc.ID(),
+			CidrBlock:        pulumi.String("10.0.3.0/24"),
+			AvailabilityZone: pulumi.String("eu-west-1b"),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("todo-private-subnet-2"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Route Table for Public Subnet
+		publicRt, err := ec2.NewRouteTable(ctx, "todo-public-rt", &ec2.RouteTableArgs{
 			VpcId: vpc.ID(),
 			Routes: ec2.RouteTableRouteArray{
 				&ec2.RouteTableRouteArgs{
@@ -56,23 +134,23 @@ func main() {
 				},
 			},
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("todo-rt"),
+				"Name": pulumi.String("todo-public-rt"),
 			},
 		})
 		if err != nil {
 			return err
 		}
 
-		// Associate Route Table with Subnet
-		_, err = ec2.NewRouteTableAssociation(ctx, "todo-rta", &ec2.RouteTableAssociationArgs{
-			SubnetId:     subnet.ID(),
-			RouteTableId: rt.ID(),
+		// Associate Public Route Table with Public Subnet
+		_, err = ec2.NewRouteTableAssociation(ctx, "todo-public-rta", &ec2.RouteTableAssociationArgs{
+			SubnetId:     publicSubnet.ID(),
+			RouteTableId: publicRt.ID(),
 		})
 		if err != nil {
 			return err
 		}
 
-		// 2. Security Groups
+		// 3. Security Groups
 		// Web SG for EC2
 		webSg, err := ec2.NewSecurityGroup(ctx, "todo-web-sg", &ec2.SecurityGroupArgs{
 			VpcId:       vpc.ID(),
@@ -130,22 +208,9 @@ func main() {
 			return err
 		}
 
-		// 3. RDS Aurora PostgreSQL
-		// Subnet Group for RDS (needs at least 2 AZs usually, creating a second subnet for this)
-		subnet2, err := ec2.NewSubnet(ctx, "todo-subnet-2", &ec2.SubnetArgs{
-			VpcId:            vpc.ID(),
-			CidrBlock:        pulumi.String("10.0.2.0/24"),
-			AvailabilityZone: pulumi.String("us-east-1b"),
-			Tags: pulumi.StringMap{
-				"Name": pulumi.String("todo-subnet-2"),
-			},
-		})
-		if err != nil {
-			return err
-		}
-
+		// 4. RDS Aurora PostgreSQL
 		dbSubnetGroup, err := rds.NewSubnetGroup(ctx, "todo-db-subnet-group", &rds.SubnetGroupArgs{
-			SubnetIds: pulumi.StringArray{subnet.ID(), subnet2.ID()},
+			SubnetIds: pulumi.StringArray{privateSubnet1.ID(), privateSubnet2.ID()},
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String("todo-db-subnet-group"),
 			},
@@ -154,13 +219,38 @@ func main() {
 			return err
 		}
 
+		// Config
+		conf := config.New(ctx, "")
+		dbUsername := conf.Get("dbUsername")
+		if dbUsername == "" {
+			dbUsername = "postgres"
+		}
+		imageTag := conf.Get("imageTag")
+		if imageTag == "" {
+			imageTag = "latest"
+		}
+		dockerUsername := conf.Require("dockerUsername")
+		dockerPassword := conf.RequireSecret("dockerPassword")
+
+		fmt.Println("Docker Username: ", dockerUsername)
+		fmt.Println("Docker Password: ", dockerPassword)
+		// Generate Random Password
+		dbPassword, err := random.NewRandomPassword(ctx, "db-password", &random.RandomPasswordArgs{
+			Length:          pulumi.Int(16),
+			Special:         pulumi.Bool(true),
+			OverrideSpecial: pulumi.String("_%"),
+		})
+		if err != nil {
+			return err
+		}
+
 		cluster, err := rds.NewCluster(ctx, "todo-db-cluster", &rds.ClusterArgs{
-			Engine:              pulumi.String("aurora-postgresql"),
+			Engine:              rds.EngineTypeAuroraPostgresql,
 			EngineMode:          pulumi.String("provisioned"),
-			EngineVersion:       pulumi.String("13.6"), // Check available versions
+			EngineVersion:       pulumi.String("15.6"),
 			DatabaseName:        pulumi.String("todoapp"),
-			MasterUsername:      pulumi.String("postgres"),
-			MasterPassword:      pulumi.String("changeme123"), // TODO: Use secrets
+			MasterUsername:      pulumi.String(dbUsername),
+			MasterPassword:      dbPassword.Result,
 			SkipFinalSnapshot:   pulumi.Bool(true),
 			VpcSecurityGroupIds: pulumi.StringArray{dbSg.ID()},
 			DbSubnetGroupName:   dbSubnetGroup.Name,
@@ -176,31 +266,21 @@ func main() {
 		_, err = rds.NewClusterInstance(ctx, "todo-db-instance", &rds.ClusterInstanceArgs{
 			ClusterIdentifier: cluster.ID(),
 			InstanceClass:     pulumi.String("db.serverless"),
-			Engine:            cluster.Engine,
+			Engine:            rds.EngineTypeAuroraPostgresql,
 			EngineVersion:     cluster.EngineVersion,
 		})
 		if err != nil {
 			return err
 		}
 
-		// 4. EC2 Instance with Docker UserData
-		userData := `#!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-`
-		// AMI for Amazon Linux 2
+		// 5. EC2 Instance (Public Subnet, No UserData)
 		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
 			MostRecent: pulumi.BoolRef(true),
 			Owners:     []string{"amazon"},
 			Filters: []ec2.GetAmiFilter{
 				{
 					Name:   "name",
-					Values: []string{"amzn2-ami-hvm-*-x86_64-gp2"},
+					Values: []string{"al2023-ami-2023.*-x86_64"},
 				},
 			},
 		})
@@ -208,16 +288,49 @@ sudo chmod +x /usr/local/bin/docker-compose
 			return err
 		}
 
-		server, err := ec2.NewInstance(ctx, "todo-server", &ec2.InstanceArgs{
+		server, err := ec2.NewInstance(ctx, "todo-server-v2", &ec2.InstanceArgs{
 			InstanceType:        pulumi.String("t3.micro"),
 			VpcSecurityGroupIds: pulumi.StringArray{webSg.ID()},
 			Ami:                 pulumi.String(ami.Id),
-			SubnetId:            subnet.ID(),
-			UserData:            pulumi.String(userData),
+			SubnetId:            publicSubnet.ID(),
+			KeyName:             keyPair.KeyName,
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("todo-server"),
+				"Name": pulumi.String("todo-server-v2"),
 			},
 		})
+		if err != nil {
+			return err
+		}
+
+		// Generate Django Secret Key
+		djangoSecret, err := random.NewRandomPassword(ctx, "django-secret", &random.RandomPasswordArgs{
+			Length:  pulumi.Int(50),
+			Special: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+
+		// 6. Run Ansible Playbook
+		_, err = local.NewCommand(ctx, "run-ansible", &local.CommandArgs{
+			Create: pulumi.Sprintf("sleep 60; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -u ec2-user --private-key %s -i '%s,' -e 'database_url=postgres://%s:%s@%s/%s' -e 'secret_key=%s' -e 'image_name=singaravelan21/todo-backend' -e 'image_tag=%s' -e 'docker_username=%s' -e 'docker_password=%s' ansible/playbook.yml",
+				privateKeyPath,
+				server.PublicIp,
+				cluster.MasterUsername,
+				dbPassword.Result,
+				cluster.Endpoint,
+				cluster.DatabaseName,
+				djangoSecret.Result,
+				imageTag,
+				dockerUsername,
+				dockerPassword,
+			),
+			Triggers: pulumi.Array{
+				server.PublicIp,
+				djangoSecret.Result,
+				dockerPassword,
+			},
+		}, pulumi.DependsOn([]pulumi.Resource{server}))
 		if err != nil {
 			return err
 		}
@@ -226,6 +339,14 @@ sudo chmod +x /usr/local/bin/docker-compose
 		ctx.Export("publicIp", server.PublicIp)
 		ctx.Export("publicHostName", server.PublicDns)
 		ctx.Export("dbEndpoint", cluster.Endpoint)
+		ctx.Export("dbUsername", cluster.MasterUsername)
+		ctx.Export("dbPassword", cluster.MasterPassword)
+		ctx.Export("dbConnectionString", pulumi.Sprintf("postgres://%s:%s@%s/%s",
+			cluster.MasterUsername,
+			dbPassword.Result,
+			cluster.Endpoint,
+			cluster.DatabaseName,
+		))
 
 		return nil
 	})
